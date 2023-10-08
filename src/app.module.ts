095dbc51beee9038bb21d6fc476869872bd2b8a3
alahirajeffrey <alahirajeffrey@gmail.com>
2023-10-08 21:19:47 +0100

    setup winston logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,30 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { EmailNotificationModule } from './email-notification/email-notification.module';
 import { ConfigModule } from '@nestjs/config';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
 
 @Module({
   imports: [
     AuthModule,
     EmailNotificationModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    WinstonModule.forRoot({
+      // options
+      transports: [
+        new winston.transports.Console({}),
+        new winston.transports.File({
+          dirname: 'logs',
+          filename: 'logs.log',
+        }),
+      ],
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(({ timestamp, level, message }) => {
+          return `[${timestamp} ${level}: ${message}]`;
+        }),
+      ),
+    }),
   ],
   controllers: [],
   providers: [],
