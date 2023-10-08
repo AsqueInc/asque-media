7b5006bc5559db318119e78a4935a3993a1c1f1c
alahirajeffrey <alahirajeffrey@gmail.com>
2023-10-08 21:09:17 +0100

    setup config anf email notification modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { EmailNotificationModule } from './email-notification/email-notification.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    EmailNotificationModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [],
   providers: [],
 })
