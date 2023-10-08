05be6b32d11811aa2033f5ecfefb61b116bf4e25
alahirajeffrey <alahirajeffrey@gmail.com>
2023-10-08 18:58:20 +0100

    setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // setup swagger
+  const config = new DocumentBuilder()
+    .setTitle('Asque Media')
+    .setDescription('Asque Media API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api-doc', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
