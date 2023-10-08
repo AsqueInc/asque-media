ef8d3dc960e172cee77cfe7a62976ce1507a1231
alahirajeffrey <alahirajeffrey@gmail.com>
2023-10-08 21:04:22 +0100

    setup otp email notification

---
+++ src/email-notification/email-notification.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { createTransport } from 'nodemailer';
+import * as Mail from 'nodemailer/lib/mailer';
+import { SendOtpEmailDto } from './dto/send-otp.dto';
+
+@Injectable()
+export class EmailNotificationService {
+  private nodeMailerTransport: Mail;
+  constructor(private readonly configService: ConfigService) {
+    //setup mail service provider
+    this.nodeMailerTransport = createTransport({
+      service: configService.get('EMAIL_SERVICE'),
+      auth: {
+        type: 'OAuth2',
+        user: configService.get('USER_EMAIL'),
+        clientId: configService.get('EMAIL_CLIENT_ID'),
+        clientSecret: configService.get('EMAIL_CLIENT_SECRET'),
+        refreshToken: configService.get('EMAIL_REFRESH_TOKEN'),
+      },
+    });
+  }
+
+  /**
+   * function to send otp email
+   * @param dto : send email dto
+   */
+  async sendOtpEmail(dto: SendOtpEmailDto) {
+    const mailOptions = {
+      from: 'alahirajeffrey@gmail.com',
+      to: dto.to,
+      subject: 'Verification Otp',
+      text: `Hi there, Here is your verification otp ${dto.otp}`,
+    };
+
+    await this.nodeMailerTransport
+      .sendMail(mailOptions)
+      .then((response) => {
+        // this.logger.info(`Email sent: ${response.response}`);
+        console.log(response);
+      })
+      .catch((error) => {
+        // this.logger.error(error);
+        throw error;
+      });
+  }
+}
