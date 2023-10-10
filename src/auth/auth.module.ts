6c4ef4ddf8f0c513d15ec774348b124a2f7f7e8a
alahirajeffrey <alahirajeffrey@gmail.com>
2023-10-10 22:04:47 +0100

    imported email notification service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,11 +4,17 @@ import { AuthService } from './auth.service';
 import { PrismaService } from 'src/prisma.service';
 import { JwtModule } from '@nestjs/jwt';
 import { UtilService } from 'src/utils/util.service';
+import { EmailNotificationService } from 'src/email-notification/email-notification.service';
 // import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, PrismaService, UtilService],
+  providers: [
+    AuthService,
+    PrismaService,
+    UtilService,
+    EmailNotificationService,
+  ],
   imports: [JwtModule.register({})],
 })
 export class AuthModule {}
