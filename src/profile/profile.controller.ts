908ba9afbe96d4085c497206ab5421c53d6096c6
alahirajeffrey <alahirajeffrey@gmail.com>
2023-10-10 20:07:53 +0100

    added profile controllers

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { UpdateProfileDto } from './dto/update-profile.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('profile-endpoints')
 @Controller('profile')
 export class ProfileController {
   constructor(private readonly profileService: ProfileService) {}
@@ -12,12 +14,17 @@ export class ProfileController {
     return this.profileService.createProfile(dto);
   }
 
-  @Patch('update-profile/:id')
+  @Patch('update-profile/:userId/:profileId')
   updateProfile(
     @Body() dto: UpdateProfileDto,
     @Param('userId') userId: string,
     @Param('profileId') profileId: string,
   ) {
     return this.profileService.updatedProfile(dto, userId, profileId);
+  }
+
+  @Get(':profileId')
+  getProfile(@Param('profileId') profileId: string) {
+    return this.profileService.getProfile(profileId);
   }
 }
