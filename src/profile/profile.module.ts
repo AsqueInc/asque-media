86454151fb15a50f02edd555db93b24db440615c
alahirajeffrey <alahirajeffrey@gmail.com>
2023-10-10 20:08:59 +0100

    updated profle module

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { ProfileController } from './profile.controller';
 import { PrismaService } from 'src/prisma.service';
+import { UtilService } from 'src/utils/util.service';
 
 @Module({
-  providers: [ProfileService, PrismaService],
+  providers: [ProfileService, PrismaService, UtilService],
   controllers: [ProfileController],
 })
 export class ProfileModule {}
