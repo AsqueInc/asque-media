5a608bc8aa0fd2789c316be6f2af66beb530f4e3
alahirajeffrey <alahirajeffrey@gmail.com>
2023-10-10 22:01:20 +0100

    imported util service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,12 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaService } from 'src/prisma.service';
 import { JwtModule } from '@nestjs/jwt';
+import { UtilService } from 'src/utils/util.service';
 // import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, PrismaService],
+  providers: [AuthService, PrismaService, UtilService],
   imports: [JwtModule.register({})],
 })
 export class AuthModule {}
