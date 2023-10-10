85b28e546a90cde7084ce5f61afcaaa119f35ef7
alahirajeffrey <alahirajeffrey@gmail.com>
2023-10-10 20:07:33 +0100

    setup authorization in swagger

--- src/main.ts
+++ src/main.ts
@@ -19,8 +19,19 @@ async function bootstrap() {
     .setTitle('Asque Media')
     .setDescription('Asque Media API description')
     .setVersion('1.0')
-    .addBearerAuth()
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth',
+    )
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-doc', app, document);
 
