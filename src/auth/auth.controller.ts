0ced0232ae14a6aa65a04c062c6dccf29c5f1699
alahirajeffrey <alahirajeffrey@gmail.com>
2023-10-10 14:11:22 +0100

    code refactor

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,17 @@
-import { Body, Controller, Param, Post, Patch } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  Post,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { LoginDto } from './dto/login.dto';
 import { ChangePasswordDto } from './dto/change-password.dto';
+import { AuthGuard } from './guards/auth.guard';
 
 @ApiTags('auth-endpoints')
 @Controller('auth')
@@ -20,6 +28,8 @@ export class AuthController {
     return this.authService.loginUser(dto);
   }
 
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth('token')
   @Patch('change-password/:id')
   changePassword(@Body() dto: ChangePasswordDto, @Param('id') id: string) {
     return this.authService.changePassword(dto, id);
