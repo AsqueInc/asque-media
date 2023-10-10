0facef0c716b823b126ee74c6f27e2f076e8cf7c
alahirajeffrey <alahirajeffrey@gmail.com>
2023-10-10 14:18:16 +0100

    added create profile controller

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Param, Patch, Post } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { CreateProfileDto } from './dto/create-profile.dto';
+import { UpdateProfileDto } from './dto/update-profile.dto';
 
 @Controller('profile')
 export class ProfileController {
@@ -9,5 +10,14 @@ export class ProfileController {
   @Post('create-profile')
   createProfile(@Body() dto: CreateProfileDto) {
     return this.profileService.createProfile(dto);
+  }
+
+  @Patch('update-profile/:id')
+  updateProfile(
+    @Body() dto: UpdateProfileDto,
+    @Param('userId') userId: string,
+    @Param('profileId') profileId: string,
+  ) {
+    return this.profileService.updatedProfile(dto, userId, profileId);
   }
 }
