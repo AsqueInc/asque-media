1f25eb3644d49192e9b1db827f15656e4fba1f64
alahirajeffrey <alahirajeffrey@gmail.com>
2023-10-11 07:30:33 +0100

    imported jwt module

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -3,9 +3,11 @@ import { ProfileService } from './profile.service';
 import { ProfileController } from './profile.controller';
 import { PrismaService } from 'src/prisma.service';
 import { UtilService } from 'src/utils/util.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   providers: [ProfileService, PrismaService, UtilService],
   controllers: [ProfileController],
+  imports: [JwtModule.register({})],
 })
 export class ProfileModule {}
