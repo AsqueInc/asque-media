887a6e3b0a76ff0ac4cd58d2e6b6f4bd5a0ef61e
alahirajeffrey <alahirajeffrey@gmail.com>
2023-10-11 07:30:24 +0100

    protected routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,20 @@
-import { Body, Controller, Param, Post, Patch } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  Post,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiSecurity, ApiTags } from '@nestjs/swagger';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { LoginDto } from './dto/login.dto';
 import { ChangePasswordDto } from './dto/change-password.dto';
 import { VerifyEmailDto } from './dto/verify-email.otp';
 import { SendResetPasswordEmailDto } from './dto/send-reset-email.dto';
 import { ResetPasswordDto } from './dto/reset-password.dto';
+import { AuthGuard } from './guards/auth.guard';
 
 @ApiTags('auth-endpoints')
 @Controller('auth')
@@ -23,36 +31,31 @@ export class AuthController {
     return this.authService.loginUser(dto);
   }
 
-  // @UseGuards(AuthGuard)
-  // @ApiBearerAuth('token')
-  @Patch('change-password/:id')
+  @UseGuards(AuthGuard)
+  @ApiSecurity('JWT-auth')
   changePassword(@Body() dto: ChangePasswordDto, @Param('id') id: string) {
     return this.authService.changePassword(dto, id);
   }
 
-  // @UseGuards(AuthGuard)
-  @ApiBearerAuth('token')
+  @UseGuards(AuthGuard)
+  @ApiSecurity('JWT-auth')
   @Post('request-email-verification/:userId')
   requestEmailVerification(@Param('userId') userId: string) {
     return this.authService.requestEmailVerification(userId);
   }
 
-  // @UseGuards(AuthGuard)
-  // @ApiBearerAuth('token')
+  @UseGuards(AuthGuard)
+  @ApiSecurity('JWT-auth')
   @Patch('verify-email/:userId')
   verifyEmail(@Body() dto: VerifyEmailDto, @Param('userId') userId: string) {
     return this.authService.verifyEmail(userId, dto);
   }
 
-  // @UseGuards(AuthGuard)
-  // @ApiBearerAuth('token')
   @Post('send-reset-password-email')
   requestForgotPasswordEmail(@Body() dto: SendResetPasswordEmailDto) {
     return this.authService.requestForgotPasswordEmail(dto);
   }
 
-  // @UseGuards(AuthGuard)
-  // @ApiBearerAuth('token')
   @Patch('reset-password')
   resetPassword(@Body() dto: ResetPasswordDto) {
     return this.authService.resetPassword(dto);
--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,21 +1,32 @@
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { UpdateProfileDto } from './dto/update-profile.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiSecurity, ApiTags } from '@nestjs/swagger';
 import { RequestMobileVerificationDto } from './dto/request-mobile-verification.dto';
 import { VerifyMobileDto } from './dto/verify-mobile.dto';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
 
 @ApiTags('profile-endpoints')
 @Controller('profile')
 export class ProfileController {
   constructor(private readonly profileService: ProfileService) {}
 
+  @ApiSecurity('JWT-auth')
   @Post('create-profile')
   createProfile(@Body() dto: CreateProfileDto) {
     return this.profileService.createProfile(dto);
   }
 
+  @ApiSecurity('JWT-auth')
   @Patch('update-profile/:userId/:profileId')
   updateProfile(
     @Body() dto: UpdateProfileDto,
@@ -25,16 +36,20 @@ export class ProfileController {
     return this.profileService.updatedProfile(dto, userId, profileId);
   }
 
+  @ApiSecurity('JWT-auth')
+  @UseGuards(AuthGuard)
   @Get(':profileId')
   getProfile(@Param('profileId') profileId: string) {
     return this.profileService.getProfile(profileId);
   }
 
+  @ApiSecurity('JWT-auth')
   @Post('request-mobile-verification')
   requestMobileNumberVerification(@Body() dto: RequestMobileVerificationDto) {
     return this.profileService.requestMobileNumberVerification(dto);
   }
 
+  @ApiSecurity('JWT-auth')
   @Patch('verify-mobile/:userId')
   verifyMobileNumber(
     @Body() dto: VerifyMobileDto,
