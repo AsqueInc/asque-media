7d5b221b44dba03ac315a2a9b780455fff27289c
alahirajeffrey <alahirajeffrey@gmail.com>
2023-10-14 15:37:15 +0100

    added message service

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -4,9 +4,10 @@ import { ProfileController } from './profile.controller';
 import { PrismaService } from 'src/prisma.service';
 import { UtilService } from 'src/utils/util.service';
 import { JwtModule } from '@nestjs/jwt';
+import { MessageService } from 'src/utils/message.service';
 
 @Module({
-  providers: [ProfileService, PrismaService, UtilService],
+  providers: [ProfileService, PrismaService, UtilService, MessageService],
   controllers: [ProfileController],
   imports: [JwtModule.register({})],
 })
