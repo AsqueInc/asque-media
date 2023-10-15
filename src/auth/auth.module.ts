e2157560f8550d91b1d144f5dd3a9e08b707a057
alahirajeffrey <alahirajeffrey@gmail.com>
2023-10-15 12:14:24 +0100

    import jwt module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,7 +5,8 @@ import { PrismaService } from 'src/prisma.service';
 import { JwtModule } from '@nestjs/jwt';
 import { UtilService } from 'src/utils/util.service';
 import { EmailNotificationService } from 'src/email-notification/email-notification.service';
-// import { JwtStrategy } from './strategy/jwt.strategy';
+import { GoogleStrategy } from './strategy/google.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   controllers: [AuthController],
@@ -14,7 +15,11 @@ import { EmailNotificationService } from 'src/email-notification/email-notificat
     PrismaService,
     UtilService,
     EmailNotificationService,
+    GoogleStrategy,
   ],
-  imports: [JwtModule.register({})],
+  imports: [
+    JwtModule.register({}),
+    PassportModule.register({ defaultStrategy: 'google' }),
+  ],
 })
 export class AuthModule {}
