03bbbd131a56566ec1f444304e5f76a672da43d5
alahirajeffrey <alahirajeffrey@gmail.com>
2023-10-15 12:15:15 +0100

    protected category routes

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -6,18 +6,22 @@ import {
   Patch,
   Post,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 import { PaginationDto } from './dto/pagination.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiSecurity, ApiTags } from '@nestjs/swagger';
+import { JwtGuard } from 'src/auth/guards/auth.guard';
 
 @ApiTags('category-endpoints')
 @Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
+  @UseGuards(JwtGuard)
+  @ApiSecurity('JWT-auth')
   @Post('')
   CreateCategory(
     @Body() dto: CreateCategoryDto,
@@ -26,6 +30,8 @@ export class CategoryController {
     return this.categoryService.createCategory(dto, userId);
   }
 
+  @UseGuards(JwtGuard)
+  @ApiSecurity('JWT-auth')
   @Patch(':userId/:categoryId')
   updateCategory(
     @Body() dto: UpdateCategoryDto,
@@ -35,11 +41,15 @@ export class CategoryController {
     return this.categoryService.updateCategory(dto, userId, categoryId);
   }
 
+  @UseGuards(JwtGuard)
+  @ApiSecurity('JWT-auth')
   @Get(':categoryId')
   getCategoryDetails(@Param('categoryId') categoryId: string) {
     return this.categoryService.getCategoryDetails(categoryId);
   }
 
+  @UseGuards(JwtGuard)
+  @ApiSecurity('JWT-auth')
   @Get('all/:categoryId')
   getAllArtInCategory(
     @Query() dto: PaginationDto,
