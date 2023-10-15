9e7fbf41bbb44b2186d019009abd4b386799bd4b
alahirajeffrey <alahirajeffrey@gmail.com>
2023-10-15 12:15:56 +0100

    imported jwt module

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -3,9 +3,11 @@ import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
 import { PrismaService } from 'src/prisma.service';
 import { EmailNotificationService } from 'src/email-notification/email-notification.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   providers: [CategoryService, PrismaService, EmailNotificationService],
   controllers: [CategoryController],
+  imports: [JwtModule.register({})],
 })
 export class CategoryModule {}
