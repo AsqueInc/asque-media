64fa8aea6e752997f88c7784c030ad81de59c0b7
alahirajeffrey <alahirajeffrey@gmail.com>
2023-10-22 14:44:12 +0100

    setup functionality for image upload

--- src/utils/file-upload.service.ts
+++ src/utils/file-upload.service.ts
@@ -17,24 +17,43 @@ export class FileUploadService {
     });
   }
 
-  async uploadProfilePicture(file: Express.Multer.File) {
+  async uploadPicture(file: Express.Multer.File, uploadType: ImageUploadType) {
     try {
       if (file.size > 3000000) {
         throw new Error('Please upload a file that is not more than 3MB');
       }
 
-      if (!file.mimetype.startsWith('iamge')) {
+      if (!file.mimetype.startsWith('image')) {
         throw new Error('You can only upload images');
       }
 
-      const uploadedFile = await cloudinary.v2.uploader.upload(file.path, {
-        folder: 'uploads/profile-picture', // Set the folder in Cloudinary where the file will be stored
-        transformation: [{ height: 170, width: 170 }, { radius: 20 }],
-      });
+      if (uploadType === ImageUploadType.ProfilePicture) {
+        const uploadedFile = await cloudinary.v2.uploader.upload(file.path, {
+          folder: 'uploads/profile-picture', // Set the folder in Cloudinary where the file will be stored
+          transformation: [{ height: 170, width: 170 }, { radius: 20 }],
+        });
 
-      this.logger.info(uploadedFile.public_id);
+        this.logger.info(uploadedFile.public_id);
+        return { imagePublicId: uploadedFile.public_id };
+      }
+
+      if (uploadType === ImageUploadType.Image) {
+        const uploadedFile = await cloudinary.v2.uploader.upload(file.path, {
+          folder: 'uploads/images', // Set the folder in Cloudinary where the file will be stored
+          transformation: [{ height: 300, width: 300 }],
+        });
+
+        // remove log after testing
+        this.logger.info(uploadedFile.public_id);
+        return { imagePublicId: uploadedFile.public_id };
+      }
     } catch (error) {
       throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
+}
+
+export enum ImageUploadType {
+  ProfilePicture,
+  Image,
 }
