4298e5a63dc0d85c1d634230db85dd2d119bbd56
alahirajeffrey <alahirajeffrey@gmail.com>
2023-10-24 12:00:58 +0100

    added dto to create review

---
+++ src/reviews/dto/create-review.dto.ts
@@ -0,0 +1,26 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional, IsString, Max, Min } from 'class-validator';
+
+export class CreateReviewDto {
+  @IsString()
+  @ApiProperty()
+  @IsNotEmpty()
+  comment: string;
+
+  @IsString()
+  @ApiProperty()
+  @IsOptional()
+  @Min(1)
+  @Max(10)
+  rating: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  artworkId: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  profileId: string;
+}
