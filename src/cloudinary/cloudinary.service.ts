813ef3d5da4c75c87cfbbf88a36b58e200c35eda
alahirajeffrey <alahirajeffrey@gmail.com>
2023-10-24 05:13:25 +0100

    setup cloudinary service

---
+++ src/cloudinary/cloudinary.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common';
+import { UploadApiErrorResponse, UploadApiResponse, v2 } from 'cloudinary';
+import toStream = require('buffer-to-stream');
+@Injectable()
+export class CloudinaryService {
+  async uploadImage(
+    file: Express.Multer.File,
+  ): Promise<UploadApiResponse | UploadApiErrorResponse> {
+    return new Promise((resolve, reject) => {
+      const upload = v2.uploader.upload_stream(
+        {
+          folder: 'artwork',
+          transformation: { width: 300, height: 300, crop: 'fill' },
+        },
+        (error, result) => {
+          if (error) return reject(error);
+          resolve(result);
+        },
+      );
+
+      toStream(file.buffer).pipe(upload);
+    });
+  }
+
+  async uploadProfilePicture(
+    file: Express.Multer.File,
+  ): Promise<UploadApiResponse | UploadApiErrorResponse> {
+    return new Promise((resolve, reject) => {
+      const upload = v2.uploader.upload_stream(
+        {
+          folder: 'profile',
+          transformation: { width: 170, height: 170, crop: 'fill' },
+        },
+        (error, result) => {
+          if (error) return reject(error);
+          resolve(result);
+        },
+      );
+
+      toStream(file.buffer).pipe(upload);
+    });
+  }
+}
+
+export enum ImageUploadType {
+  ProfilePicture,
+  Image,
+}
