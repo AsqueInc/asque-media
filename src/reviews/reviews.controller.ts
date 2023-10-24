dd3a343df6d2716f7bbb341a1478077f490b7420
alahirajeffrey <alahirajeffrey@gmail.com>
2023-10-24 12:02:44 +0100

    controller to create, get, delete, update review

---
+++ src/reviews/reviews.controller.ts
@@ -0,0 +1,65 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { ReviewsService } from './reviews.service';
+import { ApiOperation, ApiSecurity, ApiTags } from '@nestjs/swagger';
+import { PaginationDto } from 'src/category/dto/pagination.dto';
+import { CreateReviewDto } from './dto/create-review.dto';
+import { UpdateReviewDto } from './dto/update-review.dto';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
+
+@UseGuards(JwtGuard)
+@ApiSecurity('JWT-auth')
+@ApiTags('review-endpoints')
+@Controller('reviews')
+export class ReviewsController {
+  constructor(private readonly reviewService: ReviewsService) {}
+
+  @Get('artwork/:artworkId')
+  @ApiOperation({ summary: 'Get all reviews of an artwork' })
+  getAllArtworkReviews(
+    @Query() dto: PaginationDto,
+    @Param('artworkId') artworkId: string,
+  ) {
+    return this.reviewService.getAllArtworkReviews(artworkId, dto);
+  }
+
+  @Get(':reviewId')
+  @ApiOperation({ summary: 'Get a single review' })
+  getSingleReview(@Param('reviewId') reviewId: string) {
+    return this.reviewService.getSingleReview(reviewId);
+  }
+
+  @Delete(':reviewId/:profileId')
+  @ApiOperation({ summary: 'Delete a review' })
+  deleteReview(
+    @Param('reviewId') reviewId: string,
+    @Param('profileId') profileId: string,
+  ) {
+    return this.reviewService.deleteReview(reviewId, profileId);
+  }
+
+  @Post('')
+  @ApiOperation({ summary: 'Create a review' })
+  createReview(@Body() dto: CreateReviewDto) {
+    return this.reviewService.createReview(dto);
+  }
+
+  @Patch(':reviewId/:profileId')
+  @ApiOperation({ summary: 'Create a review' })
+  updateReview(
+    @Body() dto: UpdateReviewDto,
+    @Param('reviewId') reviewId: string,
+    @Param('profileId') profileId: string,
+  ) {
+    return this.reviewService.updateReview(reviewId, profileId, dto);
+  }
+}
