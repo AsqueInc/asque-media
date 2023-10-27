fe2bbe164c15bae5c7ea1c43c0517c9c7f0e2022
alahirajeffrey <alahirajeffrey@gmail.com>
2023-10-27 14:44:53 +0100

    updated module

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
+import { PrismaService } from 'src/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
+import { MessageService } from 'src/utils/message.service';
 
 @Module({
   controllers: [OrderController],
-  providers: [OrderService],
+  providers: [OrderService, PrismaService, MessageService],
+  imports: [JwtModule.register({})],
 })
 export class OrderModule {}
