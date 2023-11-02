9f4dd6c200216349f40035e1d3cab2255f9283c4
alahirajeffrey <alahirajeffrey@gmail.com>
2023-11-02 11:21:40 +0100

    setup payment module

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PaymentController } from './payment.controller';
 import { PaymentService } from './payment.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [PaymentController],
-  providers: [PaymentService]
+  providers: [PaymentService, PrismaService],
+  imports: [JwtModule.register({})],
 })
 export class PaymentModule {}
