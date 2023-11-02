bc5e794f66fc327001530899089bab7aa81bd328
alahirajeffrey <alahirajeffrey@gmail.com>
2023-11-02 11:21:26 +0100

    added controllers to create and verify payment

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -1,6 +1,28 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Param, Post, UseGuards } from '@nestjs/common';
+import { ApiOperation, ApiSecurity, ApiTags } from '@nestjs/swagger';
+import { PaymentService } from './payment.service';
+import { ProcessPaymentDto } from './dto/process-payment.dto';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
 
-@ApiTags('payment-controller')
+@UseGuards(JwtGuard)
+@ApiSecurity('JWT-auth')
+@ApiTags('payment-endpoints')
 @Controller('payment')
-export class PaymentController {}
+export class PaymentController {
+  constructor(private readonly paymentService: PaymentService) {}
+
+  @Post('')
+  @ApiOperation({ summary: 'Process payment for an order' })
+  processPayment(@Body() dto: ProcessPaymentDto) {
+    return this.paymentService.processPayment(dto);
+  }
+
+  @Post('verify/:reference/:orderId')
+  @ApiOperation({ summary: 'Verify the status of a payment' })
+  registerUser(
+    @Param('reference') reference: string,
+    @Param('orderId') orderId: string,
+  ) {
+    return this.paymentService.verifyPayment(reference, orderId);
+  }
+}
