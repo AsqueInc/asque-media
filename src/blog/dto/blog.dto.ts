6934228d773018c2d704cef27e5c0e6d662b335d
alahirajeffrey <alahirajeffrey@gmail.com>
2023-12-04 10:29:09 +0100

    dtos to create and update blog

---
+++ src/blog/dto/blog.dto.ts
@@ -0,0 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateBlogDto {
+  @IsString()
+  @ApiProperty()
+  @IsNotEmpty()
+  title: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  content: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  userId: string;
+}
+
+export class UpdateBlogDto {
+  @IsString()
+  @ApiProperty()
+  @IsOptional()
+  title: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  content: string;
+}
