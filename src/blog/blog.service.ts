29def14b00199eb0b505c4f601324ebc70f6905c
alahirajeffrey <alahirajeffrey@gmail.com>
2023-12-04 10:47:49 +0100

    added service to upload image to blog, refactor

--- src/blog/blog.service.ts
+++ src/blog/blog.service.ts
@@ -3,12 +3,14 @@ import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { PrismaService } from 'src/prisma.service';
 import { Logger } from 'winston';
 import { CreateBlogDto, UpdateBlogDto } from './dto/blog.dto';
+import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
 
 @Injectable()
 export class BlogService {
   constructor(
     private prisma: PrismaService,
     @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+    private cloudinary: CloudinaryService,
   ) {}
 
   async createBlog(dto: CreateBlogDto) {
@@ -17,9 +19,9 @@ export class BlogService {
         data: {
           title: dto.title,
           content: dto.content,
-          user: {
+          profile: {
             connect: {
-              id: dto.userId,
+              id: dto.profileId,
             },
           },
         },
@@ -38,10 +40,10 @@ export class BlogService {
     }
   }
 
-  async getAllUserBlogs(userId: string) {
+  async getAllUserBlogs(profileId: string) {
     try {
       const blogs = await this.prisma.blog.findMany({
-        where: { userId: userId },
+        where: { profileId: profileId },
       });
 
       return {
@@ -77,19 +79,21 @@ export class BlogService {
     }
   }
 
-  async deleteBlog(userId: string, blogId: string) {
+  async deleteBlog(profileId: string, blogId: string) {
     try {
       const blog = await this.prisma.blog.findFirst({ where: { id: blogId } });
       if (!blog) {
         throw new HttpException('Blog does not exists', HttpStatus.NOT_FOUND);
       }
 
-      const user = await this.prisma.user.findFirst({ where: { id: userId } });
-      if (!user) {
+      const profile = await this.prisma.profile.findFirst({
+        where: { id: profileId },
+      });
+      if (!profile) {
         throw new HttpException('User does not exists', HttpStatus.NOT_FOUND);
       }
 
-      if (blog.userId !== userId) {
+      if (blog.profileId !== profileId) {
         throw new HttpException(
           'You cannot delete a blog that you did not write',
           HttpStatus.UNAUTHORIZED,
@@ -111,19 +115,21 @@ export class BlogService {
     }
   }
 
-  async updateBlog(userId: string, blogId: string, dto: UpdateBlogDto) {
+  async updateBlog(profileId: string, blogId: string, dto: UpdateBlogDto) {
     try {
       const blog = await this.prisma.blog.findFirst({ where: { id: blogId } });
       if (!blog) {
         throw new HttpException('Blog does not exists', HttpStatus.NOT_FOUND);
       }
 
-      const user = await this.prisma.user.findFirst({ where: { id: userId } });
-      if (!user) {
+      const profile = await this.prisma.profile.findFirst({
+        where: { id: profileId },
+      });
+      if (!profile) {
         throw new HttpException('User does not exists', HttpStatus.NOT_FOUND);
       }
 
-      if (blog.userId !== userId) {
+      if (blog.profileId !== profileId) {
         throw new HttpException(
           'You cannot update a blog that you did not write',
           HttpStatus.UNAUTHORIZED,
@@ -136,6 +142,46 @@ export class BlogService {
           title: dto.title,
           content: dto.content,
         },
+      });
+
+      return {
+        statusCode: HttpStatus.OK,
+        message: { updatedBlog },
+      };
+    } catch (error) {
+      this.logger.error(error);
+      throw new HttpException(
+        error.message,
+        error.status || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async addImageToBlog(
+    blogId: string,
+    profileId: string,
+    file: Express.Multer.File,
+  ) {
+    try {
+      const blog = await this.prisma.blog.findFirst({ where: { id: blogId } });
+      if (!blog) {
+        throw new HttpException('Blog does not exists', HttpStatus.NOT_FOUND);
+      }
+
+      if (blog.profileId !== profileId) {
+        throw new HttpException(
+          'You cannot upload an image to an blog you did not write',
+          HttpStatus.UNAUTHORIZED,
+        );
+      }
+
+      const blogImageUriList = blog.imageUris;
+      const uploadedImageUri = await this.cloudinary.uploadImage(file);
+      blogImageUriList.push(uploadedImageUri.url);
+
+      const updatedBlog = await this.prisma.blog.update({
+        where: { id: blogId },
+        data: { imageUris: blogImageUriList },
       });
 
       return {
