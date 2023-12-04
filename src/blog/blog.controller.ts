4c4ff87d6ef0e5c07fdeb9f600b4787d022904a3
alahirajeffrey <alahirajeffrey@gmail.com>
2023-12-04 10:29:30 +0100

    added basic blog controllers

---
+++ src/blog/blog.controller.ts
@@ -0,0 +1,52 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Delete,
+  Patch,
+} from '@nestjs/common';
+import { BlogService } from './blog.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CreateBlogDto, UpdateBlogDto } from './dto/blog.dto';
+
+@ApiTags('blog-endpoints')
+@Controller('blog')
+export class BlogController {
+  constructor(private readonly blogService: BlogService) {}
+
+  @Get(':blogId')
+  @ApiOperation({ summary: 'Get a single blog by id' })
+  getProfile(@Param('blogId') blogId: string) {
+    return this.blogService.getSingleBlog(blogId);
+  }
+
+  @Post('')
+  @ApiOperation({ summary: 'Create a blog' })
+  createProfile(@Body() dto: CreateBlogDto) {
+    return this.blogService.createBlog(dto);
+  }
+
+  @Get('user/:userId')
+  @ApiOperation({ summary: 'Get all blogs by a user' })
+  getAllUserBlogs(@Param('userId') userId: string) {
+    return this.blogService.getAllUserBlogs(userId);
+  }
+
+  @Delete(':userId/:blogId')
+  @ApiOperation({ summary: 'Delete a blog' })
+  deleteBlog(@Param('userId') userId: string, @Param('blogId') blogId: string) {
+    return this.blogService.deleteBlog(userId, blogId);
+  }
+
+  @Patch(':userId/:blogId')
+  @ApiOperation({ summary: 'Update a blog' })
+  updateBlog(
+    @Param('userId') userId: string,
+    @Param('blogId') blogId: string,
+    @Body() dto: UpdateBlogDto,
+  ) {
+    return this.blogService.updateBlog(userId, blogId, dto);
+  }
+}
