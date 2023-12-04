85453169b9adc828955e7bf1a66b2457107c2e02
alahirajeffrey <alahirajeffrey@gmail.com>
2023-12-04 10:47:33 +0100

    added cloudinary module

--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -3,10 +3,11 @@ import { BlogController } from './blog.controller';
 import { BlogService } from './blog.service';
 import { PrismaService } from 'src/prisma.service';
 import { JwtModule } from '@nestjs/jwt';
+import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
 
 @Module({
   controllers: [BlogController],
   providers: [BlogService, PrismaService],
-  imports: [JwtModule.register({})],
+  imports: [JwtModule.register({}), CloudinaryModule],
 })
 export class BlogModule {}
