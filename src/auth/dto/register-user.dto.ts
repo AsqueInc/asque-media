921e57c869f74f6d609f7f900d5202dd2ade96a6
alahirajeffrey <alahirajeffrey@gmail.com>
2024-01-26 21:21:59 +0100

    updated dto

--- src/auth/dto/register-user.dto.ts
+++ src/auth/dto/register-user.dto.ts
@@ -18,7 +18,7 @@ export class RegisterUserDto {
   @IsString()
   @IsNotEmpty()
   @ApiProperty()
-  username: string;
+  name: string;
 
   @IsString()
   @IsNotEmpty()
@@ -37,20 +37,20 @@ export class RegisterUserDto {
   })
   password: string;
 
-  @IsString()
-  @IsNotEmpty()
-  @ApiProperty()
-  @MinLength(8, {
-    message:
-      'Password is too short. Minimal length is $constraint1 characters, but actual is $value',
-  })
-  @MaxLength(20, {
-    message:
-      'password is too long. Maximal length is $constraint1 characters, but actual is $value',
-  })
-  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?!.*\s).{8,20}$/, {
-    message:
-      'password must contain the following: a capital letter, a small letter, and a number',
-  })
-  confirmPassword: string;
+  // @IsString()
+  // @IsNotEmpty()
+  // @ApiProperty()
+  // @MinLength(8, {
+  //   message:
+  //     'Password is too short. Minimal length is $constraint1 characters, but actual is $value',
+  // })
+  // @MaxLength(20, {
+  //   message:
+  //     'password is too long. Maximal length is $constraint1 characters, but actual is $value',
+  // })
+  // @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?!.*\s).{8,20}$/, {
+  //   message:
+  //     'password must contain the following: a capital letter, a small letter, and a number',
+  // })
+  // confirmPassword: string;
 }
