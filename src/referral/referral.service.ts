0c4a5bd3221458f7c95f193b9725ff4eab670f34
alahirajeffrey <alahirajeffrey@gmail.com>
2024-01-28 11:18:51 +0100

    added service to get referral details

--- src/referral/referral.service.ts
+++ src/referral/referral.service.ts
@@ -1,4 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+  Logger,
+} from '@nestjs/common';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class ReferralService {}
+export class ReferralService {
+  constructor(
+    private prisma: PrismaService,
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+  ) {}
+
+  async getReferralDetailsViaEmail(email: string) {
+    try {
+      const referral = await this.prisma.referral.findFirst({
+        where: { userEmail: email },
+      });
+
+      if (!referral) {
+        throw new HttpException(
+          'Referral details not found',
+          HttpStatus.NOT_FOUND,
+        );
+      }
+
+      return {
+        statusCode: HttpStatus.OK,
+        data: referral,
+      };
+    } catch (error) {
+      this.logger.error(error);
+      throw new HttpException(
+        error.message,
+        error.status || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async getReferralDetailsViaCode(code: string) {
+    try {
+      const referral = await this.prisma.referral.findFirst({
+        where: { code: code },
+      });
+
+      if (!referral) {
+        throw new HttpException(
+          'Referral details not found',
+          HttpStatus.NOT_FOUND,
+        );
+      }
+
+      return {
+        statusCode: HttpStatus.OK,
+        data: referral,
+      };
+    } catch (error) {
+      this.logger.error(error);
+      throw new HttpException(
+        error.message,
+        error.status || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+}
