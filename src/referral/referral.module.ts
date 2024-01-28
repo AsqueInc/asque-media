1394397f60eaa8af61d82784d3262e9041dc9d98
alahirajeffrey <alahirajeffrey@gmail.com>
2024-01-28 11:19:42 +0100

    setup jwt

--- src/referral/referral.module.ts
+++ src/referral/referral.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ReferralService } from './referral.service';
 import { ReferralController } from './referral.controller';
 import { PrismaService } from 'src/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   providers: [ReferralService, PrismaService],
   controllers: [ReferralController],
+  imports: [JwtModule.register({})],
 })
 export class ReferralModule {}
