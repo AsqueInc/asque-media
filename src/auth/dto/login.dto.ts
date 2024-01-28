3d5fa9b299a7890f9f5c129f0b8ad76e5c9f27e3
alahirajeffrey <alahirajeffrey@gmail.com>
2024-01-28 09:42:07 +0100

    refactor

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,12 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  IsEmail,
-  IsNotEmpty,
-  IsString,
-  Matches,
-  MaxLength,
-  MinLength,
-} from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginDto {
   @IsString()
@@ -18,17 +11,5 @@ export class LoginDto {
   @IsString()
   @IsNotEmpty()
   @ApiProperty()
-  @MinLength(8, {
-    message:
-      'Password is too short. Minimal length is $constraint1 characters, but actual is $value',
-  })
-  @MaxLength(20, {
-    message:
-      'password is too long. Maximal length is $constraint1 characters, but actual is $value',
-  })
-  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?!.*\s).{8,20}$/, {
-    message:
-      'password must contain the following: a capital letter, a small letter, and a number',
-  })
   password: string;
 }
