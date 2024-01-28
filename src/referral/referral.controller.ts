a25013f44ced0053a136f7de01212600ce615646
alahirajeffrey <alahirajeffrey@gmail.com>
2024-01-28 11:19:04 +0100

    added controller to get referral details

--- src/referral/referral.controller.ts
+++ src/referral/referral.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common';
+import { ReferralService } from './referral.service';
+import { ApiOperation, ApiSecurity, ApiTags } from '@nestjs/swagger';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
 
+@ApiTags('referral-endpoints')
 @Controller('referral')
-export class ReferralController {}
+export class ReferralController {
+  constructor(private referralService: ReferralService) {}
+
+  @UseGuards(JwtGuard)
+  @ApiSecurity('JWT-auth')
+  @Get('email')
+  @ApiOperation({ summary: 'Get referral details via email' })
+  getReferralDetailsViaEmail(@Req() req) {
+    return this.referralService.getReferralDetailsViaEmail(req.user.email);
+  }
+
+  @Get(':code')
+  @ApiOperation({ summary: 'Get referral details via code' })
+  getReferralDetailsViaCode(@Param('code') code: string) {
+    return this.referralService.getReferralDetailsViaCode(code);
+  }
+}
