c97b0993e6c19aba446174e9d1861e3ffd74d2c6
alahirajeffrey <alahirajeffrey@gmail.com>
2024-01-30 09:44:13 +0100

    added fields from figma file to dto

--- src/profile/dto/update-profile.dto.ts
+++ src/profile/dto/update-profile.dto.ts
@@ -1,11 +1,47 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsOptional, IsString } from 'class-validator';
+import { IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class UpdateProfileDto {
   @IsString()
   @ApiProperty()
   @IsOptional()
   mobileNumber: string;
+
+  @IsString()
+  @ApiProperty()
+  @IsOptional()
+  briefBio: string;
+
+  @IsString()
+  @ApiProperty()
+  @IsOptional()
+  socialMediaHandle: string;
+
+  @IsString()
+  @ApiProperty()
+  @IsOptional()
+  bank: string;
+
+  @IsString()
+  @ApiProperty()
+  @IsOptional()
+  @MaxLength(10, {
+    message: 'Invalid account number',
+  })
+  @MinLength(10, {
+    message: 'Invalid account number',
+  })
+  accountNumber: string;
+
+  @IsString()
+  @ApiProperty()
+  @IsOptional()
+  accountName: string;
+
+  @IsString()
+  @ApiProperty()
+  @IsOptional()
+  websiteLink: string;
 
   @IsString()
   @ApiProperty()
