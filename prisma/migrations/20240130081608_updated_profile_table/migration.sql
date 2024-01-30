f9c7e8f5faa2a61c534198eb9e32534d83b0f79b
alahirajeffrey <alahirajeffrey@gmail.com>
2024-01-30 09:43:56 +0100

    updated profile scema and generated migration

---
+++ prisma/migrations/20240130081608_updated_profile_table/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "Profile" ADD COLUMN     "accountName" TEXT,
+ADD COLUMN     "accountNumber" TEXT,
+ADD COLUMN     "bank" TEXT,
+ADD COLUMN     "briefBio" TEXT,
+ADD COLUMN     "socialMediaHandle" TEXT,
+ADD COLUMN     "websiteLink" TEXT;
