05ab176e90558f351a9dff10c135cbbc8a8d2bac
alahirajeffrey <alahirajeffrey@gmail.com>
2024-01-31 09:56:43 +0100

    refactor image upload service

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -4,22 +4,42 @@ import toStream = require('buffer-to-stream');
 @Injectable()
 export class CloudinaryService {
   async uploadImage(
+    type: 'Artwork' | 'Image',
     file: Express.Multer.File,
   ): Promise<UploadApiResponse | UploadApiErrorResponse> {
-    return new Promise((resolve, reject) => {
-      const upload = v2.uploader.upload_stream(
-        {
-          folder: 'artwork',
-          transformation: { width: 300, height: 300, crop: 'fill' },
-        },
-        (error, result) => {
-          if (error) return reject(error);
-          resolve(result);
-        },
-      );
+    if (type === 'Artwork') {
+      return new Promise((resolve, reject) => {
+        const upload = v2.uploader.upload_stream(
+          {
+            folder: 'artwork',
+            transformation: { width: 300, height: 300, crop: 'fill' },
+          },
+          (error, result) => {
+            if (error) return reject(error);
+            resolve(result);
+          },
+        );
 
-      toStream(file.buffer).pipe(upload);
-    });
+        toStream(file.buffer).pipe(upload);
+      });
+    }
+
+    if (type === 'Image') {
+      return new Promise((resolve, reject) => {
+        const upload = v2.uploader.upload_stream(
+          {
+            folder: 'image',
+            transformation: { width: 300, height: 300, crop: 'fill' },
+          },
+          (error, result) => {
+            if (error) return reject(error);
+            resolve(result);
+          },
+        );
+
+        toStream(file.buffer).pipe(upload);
+      });
+    }
   }
 
   async uploadProfilePicture(
