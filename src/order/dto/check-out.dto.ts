96f2d177fdbe6d8c32e2d3c46ed10f457ca5b655
alahirajeffrey <alahirajeffrey@gmail.com>
2024-02-05 23:02:41 +0100

    updated checkout dto

--- src/order/dto/check-out.dto.ts
+++ src/order/dto/check-out.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CheckOutDto {
   @IsString()
@@ -21,4 +21,9 @@ export class CheckOutDto {
   @IsNotEmpty()
   @ApiProperty()
   country: string;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty()
+  referrerCode: string;
 }
