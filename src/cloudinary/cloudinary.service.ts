bd15ca0a4e720f4efccebf87ff182f69d191ca83
alahirajeffrey <alahirajeffrey@gmail.com>
2024-02-06 23:45:41 +0100

    code refactor

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -3,98 +3,105 @@ import { UploadApiErrorResponse, UploadApiResponse, v2 } from 'cloudinary';
 import toStream = require('buffer-to-stream');
 @Injectable()
 export class CloudinaryService {
+  /**
+   * cloudinary upload helper function
+   * @param type : type of file to upload i.e image or other
+   * @param folderName : folder where file is to be saved
+   * @param file : file object
+   * @param width : image width
+   * @param height : image heigh
+   * @param crop : crop type
+   * @returns : error or upload result
+   */
+  private async uploadHelper(
+    type: 'image' | 'other',
+    folderName: string,
+    file: Express.Multer.File,
+    width?: number,
+    height?: number,
+    crop?: string,
+  ): Promise<UploadApiResponse | UploadApiErrorResponse> {
+    if (type === 'image') {
+      return new Promise((resolve, reject) => {
+        const upload = v2.uploader.upload_stream(
+          {
+            folder: folderName,
+            transformation: { width: width, height: height, crop: crop },
+          },
+          (error, result) => {
+            if (error) return reject(error);
+            resolve(result);
+          },
+        );
+
+        toStream(file.buffer).pipe(upload);
+      });
+    }
+
+    if (type === 'other') {
+      return await new Promise((resolve, reject) => {
+        const upload = v2.uploader.upload_stream(
+          {
+            folder: folderName,
+          },
+          (error, result) => {
+            if (error) return reject(error);
+            resolve(result);
+          },
+        );
+
+        toStream(file.buffer).pipe(upload);
+      });
+    }
+  }
+
+  /**
+   * function to upload images
+   * @param type : image type i.e artwork, image or profile
+   * @param file : file object
+   * @returns : error or result
+   */
   async uploadImage(
-    type: 'Artwork' | 'Image',
+    type: 'Artwork' | 'Image' | 'Profile',
     file: Express.Multer.File,
   ): Promise<UploadApiResponse | UploadApiErrorResponse> {
     if (type === 'Artwork') {
-      return new Promise((resolve, reject) => {
-        const upload = v2.uploader.upload_stream(
-          {
-            folder: 'artwork',
-            transformation: { width: 300, height: 300, crop: 'fill' },
-          },
-          (error, result) => {
-            if (error) return reject(error);
-            resolve(result);
-          },
-        );
-
-        toStream(file.buffer).pipe(upload);
-      });
+      return await this.uploadHelper(
+        'image',
+        'artwork',
+        file,
+        300,
+        300,
+        'fill',
+      );
     }
 
     if (type === 'Image') {
-      return new Promise((resolve, reject) => {
-        const upload = v2.uploader.upload_stream(
-          {
-            folder: 'image',
-            transformation: { width: 300, height: 300, crop: 'fill' },
-          },
-          (error, result) => {
-            if (error) return reject(error);
-            resolve(result);
-          },
-        );
+      return await this.uploadHelper('image', 'image', file, 300, 300, 'fill');
+    }
 
-        toStream(file.buffer).pipe(upload);
-      });
+    if (type === 'Profile') {
+      return await this.uploadHelper(
+        'image',
+        'profile',
+        file,
+        170,
+        170,
+        'fill',
+      );
     }
   }
 
-  async uploadProfilePicture(
+  async uploadOther(
+    type: 'Audio' | 'Video',
     file: Express.Multer.File,
   ): Promise<UploadApiResponse | UploadApiErrorResponse> {
-    return new Promise((resolve, reject) => {
-      const upload = v2.uploader.upload_stream(
-        {
-          folder: 'profile',
-          transformation: { width: 170, height: 170, crop: 'fill' },
-        },
-        (error, result) => {
-          if (error) return reject(error);
-          resolve(result);
-        },
-      );
-
-      toStream(file.buffer).pipe(upload);
-    });
-  }
-
-  async uploadAudio(
-    file: Express.Multer.File,
-  ): Promise<UploadApiResponse | UploadApiErrorResponse> {
-    return new Promise((resolve, reject) => {
-      const upload = v2.uploader.upload_stream(
-        {
-          folder: 'audio',
-        },
-        (error, result) => {
-          if (error) return reject(error);
-          resolve(result);
-        },
-      );
-
-      toStream(file.buffer).pipe(upload);
-    });
-  }
-
-  async uploadVideo(
-    file: Express.Multer.File,
-  ): Promise<UploadApiResponse | UploadApiErrorResponse> {
-    return new Promise((resolve, reject) => {
-      const upload = v2.uploader.upload_stream(
-        {
-          folder: 'video',
-        },
-        (error, result) => {
-          if (error) return reject(error);
-          resolve(result);
-        },
-      );
-
-      toStream(file.buffer).pipe(upload);
-    });
+    if (type === 'Audio') {
+      return await this.uploadHelper('other', 'audio', file);
+    }
+    if (type === 'Video') {
+      return await this.uploadHelper('other', 'video', file);
+    }
   }
 }
 
