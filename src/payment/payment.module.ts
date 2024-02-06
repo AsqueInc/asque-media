ef4c3aa6da4bb01ec6fc4301f94e5589cea24514
alahirajeffrey <alahirajeffrey@gmail.com>
2024-02-06 16:11:02 +0100

    imported email notification service

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -3,10 +3,11 @@ import { PaymentController } from './payment.controller';
 import { PaymentService } from './payment.service';
 import { JwtModule } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma.service';
+import { EmailNotificationService } from 'src/email-notification/email-notification.service';
 
 @Module({
   controllers: [PaymentController],
-  providers: [PaymentService, PrismaService],
+  providers: [PaymentService, PrismaService, EmailNotificationService],
   imports: [JwtModule.register({})],
 })
 export class PaymentModule {}
