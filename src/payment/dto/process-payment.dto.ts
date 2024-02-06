1be7ec8ab892524ce3c920635dc270ae29f27ebd
alahirajeffrey <alahirajeffrey@gmail.com>
2024-02-06 16:08:34 +0100

    removed profileId field

--- src/payment/dto/process-payment.dto.ts
+++ src/payment/dto/process-payment.dto.ts
@@ -2,11 +2,6 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class ProcessPaymentDto {
-  @IsString()
-  @IsNotEmpty()
-  @ApiProperty()
-  profileId: string;
-
   @IsNumber()
   @IsNotEmpty()
   @ApiProperty()
