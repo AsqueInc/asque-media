5c4a6bee9b11d04422a3e0c08178f9dead692d90
alahirajeffrey <alahirajeffrey@gmail.com>
2024-02-08 12:12:12 +0100

    removed unused code

--- src/artwork/artwork.controller.ts
+++ src/artwork/artwork.controller.ts
@@ -1,31 +1,20 @@
 import {
   Body,
   Controller,
-  // FileTypeValidator,
   Get,
   Param,
-  // ParseFilePipe,
   Patch,
   Post,
   Query,
   Req,
-  // UploadedFile,
   UseGuards,
-  // UseInterceptors,
 } from '@nestjs/common';
 import { ArtworkService } from './artwork.service';
-import {
-  // ApiBody,
-  // ApiConsumes,
-  ApiOperation,
-  ApiSecurity,
-  ApiTags,
-} from '@nestjs/swagger';
+import { ApiOperation, ApiSecurity, ApiTags } from '@nestjs/swagger';
 import { JwtGuard } from 'src/auth/guards/jwt.guard';
 import { CreateArtworkDto } from './dto/create-artwork.dto';
 import { UpdateArtworkDto } from './dto/update-artwork.dto';
 import { PaginationDto } from 'src/category/dto/pagination.dto';
-// import { FileInterceptor } from '@nestjs/platform-express';
 
 @ApiTags('artwork-endpoints')
 @UseGuards(JwtGuard)
@@ -36,11 +25,7 @@ export class ArtworkController {
 
   @Get('own')
   @ApiOperation({ summary: 'Get all artwork produced by a user' })
-  getAllArtworkByUser(
-    // @Param('profileId') profileId: string,
-    @Query() dto: PaginationDto,
-    @Req() req,
-  ) {
+  getAllArtworkByUser(@Query() dto: PaginationDto, @Req() req) {
     return this.artWorkService.getAllArtworkByUser(req.user.profileId, dto);
   }
 
@@ -60,7 +45,6 @@ export class ArtworkController {
   @ApiOperation({ summary: 'Update artwork details' })
   updateArtWork(
     @Param('artworkId') artworkId: string,
-    // @Param('profileId') profileId: string,
     @Body() dto: UpdateArtworkDto,
     @Req() req,
   ) {
@@ -70,46 +54,4 @@ export class ArtworkController {
       dto,
     );
   }
-
-  // @Post(':artworkId/:profileId/:repositoryId')
-  // @ApiOperation({ summary: 'Add artwork to a repository' })
-  // addArtworkToRepository(
-  //   @Param('artworkId') artworkId: string,
-  //   @Param('profileId') profileId: string,
-  //   @Param('repositoryId') repositoryId: string,
-  // ) {
-  //   return this.artWorkService.addArtworkToRepository(
-  //     artworkId,
-  //     profileId,
-  //     repositoryId,
-  //   );
-  // }
-
-  // @Patch('image/upload/:artworkId')
-  // @ApiOperation({ summary: 'Upload artwork images' })
-  // @ApiConsumes('multipart/form-data')
-  // @ApiBody({
-  //   schema: {
-  //     type: 'object',
-  //     properties: { file: { type: 'string', format: 'binary' } },
-  //   },
-  // })
-  // @UseInterceptors(FileInterceptor('file'))
-  // uploadArtWorkImage(
-  //   // @Param('profileId') profileId: string,
-  //   @Param('artworkId') artworkId: string,
-  //   @Req() req,
-  //   @UploadedFile(
-  //     new ParseFilePipe({
-  //       validators: [new FileTypeValidator({ fileType: '.(png|jpeg|jpg)' })],
-  //     }),
-  //   )
-  //   file: Express.Multer.File,
-  // ) {
-  //   return this.artWorkService.uploadArtWorkImage(
-  //     req.user.profileId,
-  //     artworkId,
-  //     file,
-  //   );
-  // }
 }
