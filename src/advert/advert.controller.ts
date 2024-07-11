ff7b3a3ef53eb59a2b93b1bca73338228ca565b0
alahirajeffrey <alahirajeffrey@gmail.com>
2024-07-11 12:25:53 +0100

    added advert controllers

--- src/advert/advert.controller.ts
+++ src/advert/advert.controller.ts
@@ -1,19 +1,64 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiOperation, ApiSecurity, ApiTags } from '@nestjs/swagger';
 import { JwtGuard } from 'src/auth/guards/jwt.guard';
 import { AdvertService } from './advert.service';
 import { CreateAdvertDto } from './dto/create-advert.dto';
+import { PaginationDto } from 'src/category/dto/pagination.dto';
+import { UpdateAdvertDto } from './dto/update-advert.dto';
 
 @ApiTags('advert-endpoints')
-@UseGuards(JwtGuard)
-@ApiSecurity('JWT-auth')
 @Controller('advert')
 export class AdvertController {
   constructor(private readonly advertService: AdvertService) {}
 
+  @UseGuards(JwtGuard)
+  @ApiSecurity('JWT-auth')
   @Post('')
   @ApiOperation({ summary: 'Create an advert' })
   createAdvert(@Body() dto: CreateAdvertDto, @Req() req) {
     return this.advertService.createAdvert(req.user.profileId, dto);
+  }
+
+  @Get('')
+  @ApiOperation({ summary: 'Get all adverts' })
+  getAllAdverts(@Query() dto: PaginationDto) {
+    return this.advertService.getAllAdverts(dto);
+  }
+
+  @Get(':advertId')
+  @ApiOperation({ summary: 'Get single advert by id' })
+  getSingleAdvert(@Param('advertId') advertId: string) {
+    return this.advertService.getSingleAdvert(advertId);
+  }
+
+  @UseGuards(JwtGuard)
+  @ApiSecurity('JWT-auth')
+  @Patch(':advertId')
+  @ApiOperation({ summary: 'Update an advert' })
+  updateAdvert(
+    @Body() dto: UpdateAdvertDto,
+    @Req() req,
+    @Param('advertId') advertId: string,
+  ) {
+    return this.advertService.updateAdvert(req.user.profileId, advertId, dto);
+  }
+
+  @UseGuards(JwtGuard)
+  @ApiSecurity('JWT-auth')
+  @Delete(':advertId')
+  @ApiOperation({ summary: 'Delete an advert' })
+  deleteAdvert(@Req() req, @Param('advertId') advertId: string) {
+    return this.advertService.deleteAdvert(req.user.profileId, advertId);
   }
 }
