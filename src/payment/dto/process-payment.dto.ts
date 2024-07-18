9ea25718cc0d312dbcb6f02bf3871d4d67ca8ee6
alahirajeffrey <alahirajeffrey@gmail.com>
2024-07-18 12:55:01 +0100

    added subscribe dto and enum

--- src/payment/dto/process-payment.dto.ts
+++ src/payment/dto/process-payment.dto.ts
@@ -1,6 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
+export enum SubscriptionEnum {
+  Freemium = 'freemium',
+  Premium = 'premium',
+}
+
 export class ProcessPaymentDto {
   @IsNumber()
   @IsNotEmpty()
@@ -11,4 +16,11 @@ export class ProcessPaymentDto {
   @IsNotEmpty()
   @ApiProperty()
   orderId: string;
+}
+
+export class SubscribeDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ enum: SubscriptionEnum })
+  subscriptionPlan: SubscriptionEnum;
 }
