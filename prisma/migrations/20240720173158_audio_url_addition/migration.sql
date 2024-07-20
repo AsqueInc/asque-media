7bdbc4a7c49119bb5e2f7292b09985fbf0cda0e9
alahirajeffrey <alahirajeffrey@gmail.com>
2024-07-20 18:40:41 +0100

    generated migration for change

---
+++ prisma/migrations/20240720173158_audio_url_addition/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "Album" ADD COLUMN     "audioUrl" TEXT;
+
+-- AlterTable
+ALTER TABLE "ArtWork" ADD COLUMN     "audioUrl" TEXT;
+
+-- AlterTable
+ALTER TABLE "Story" ADD COLUMN     "audioUrl" TEXT;
