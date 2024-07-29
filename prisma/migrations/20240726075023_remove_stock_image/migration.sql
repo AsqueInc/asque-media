0c66407a73d8676223704c28f780e4dc1d329968
alahirajeffrey <alahirajeffrey@gmail.com>
2024-07-29 12:43:53 +0100

    removed migration file

--- prisma/migrations/20240726075023_remove_stock_image/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the `StockImage` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
--- DROP TABLE IF EXISTS "StockImage";
-
